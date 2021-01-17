ae603fafc35958e7d10b1bf34acb5a360ef44529
Ulrich Pech <ulrichpech@gmail.com>
2021-01-17 17:34:40 +0100

    add docker

--- docker/buildrun.sh
+++ docker/buildrun.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 FOLDER=${1}
 
+docker stop foxyros
 docker build --tag myros:foxy .
 docker run -d \
-    -it \
     --name foxyros \
     --rm \
+    --device=/dev/dri:/dev/dri \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="$FOLDER:/root/ros:rw" \
     -p 127.0.0.1:2223:22 \
-    myros:noetic
+    myros:foxy
 
+#export containerId=$(docker ps -l -q)
+#xhost +local:`docker inspect --format='' $containerId`
+#docker start $containerId
