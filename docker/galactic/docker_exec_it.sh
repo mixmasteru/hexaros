56231368e7d73e2f20dc283172a4133aa310c137
Ulrich Pech <ulrichpech@gmail.com>
2021-12-03 11:13:58 +0100

    switch to ros2 apt install

--- docker/galactic/docker_exec_it.sh
+++ docker/galactic/docker_exec_it.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-PROG=${1:-rqt}
+PROG=${1:-bash}
 CONTAINER="galacticros"
 
 export containerId=$(docker ps -fname=$CONTAINER -q)
 xhost +local:`docker inspect --format='{{ .Config.Hostname }}' $containerId`
 docker exec galacticros bash -c "cat .env" > .env
 docker exec -it --env-file .env $CONTAINER $PROG
-
-# 1 Pipe from a file
-#sudo docker exec --interactive CONTAINER_NAME /bin/bash < the_beginning.sh | tee the_beginning_output.txt`
-
-#2a Pipe by piping
-#echo "echo This is how we pipe to docker exec" | sudo docker exec --interactive CONTAINER_NAME /bin/bash -
